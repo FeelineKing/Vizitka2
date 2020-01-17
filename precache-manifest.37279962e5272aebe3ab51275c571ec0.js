171b52c611f471235a2e8c5c06d211c1b7aa37c1
Vadim <feelineKing@gmail.com>
2020-01-17 18:33:53 +0200

    Updates

---
+++ precache-manifest.37279962e5272aebe3ab51275c571ec0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea40c50f450dbdbaaeed3e5a23e380d9",
+    "url": "/Vizitka2/index.html"
+  },
+  {
+    "revision": "070d0538e5d8742cf5d6",
+    "url": "/Vizitka2/static/css/main.f504818e.chunk.css"
+  },
+  {
+    "revision": "271ca0266d9a184d63dc",
+    "url": "/Vizitka2/static/js/2.d5578c13.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Vizitka2/static/js/2.d5578c13.chunk.js.LICENSE"
+  },
+  {
+    "revision": "070d0538e5d8742cf5d6",
+    "url": "/Vizitka2/static/js/main.f9c94c7b.chunk.js"
+  },
+  {
+    "revision": "e0c96cfe88c9a9c13ad4",
+    "url": "/Vizitka2/static/js/runtime-main.20e1c77b.js"
+  },
+  {
+    "revision": "017502941f7e2010dbb3b135a74510ed",
+    "url": "/Vizitka2/static/media/headerBackground.01750294.png"
+  },
+  {
+    "revision": "be18ac7f8c8d695bfbcb7cb7ee91c0d7",
+    "url": "/Vizitka2/static/media/headerPerson.be18ac7f.png"
+  },
+  {
+    "revision": "91095094a4a470669a85aeb2326ccc36",
+    "url": "/Vizitka2/static/media/logo.91095094.png"
+  },
+  {
+    "revision": "1aa460bf4f7fb0dd0f7936b6a533baa6",
+    "url": "/Vizitka2/static/media/myPhoto.1aa460bf.JPG"
+  },
+  {
+    "revision": "bd1f3ced3c67e1fd2987e42bd2fe2094",
+    "url": "/Vizitka2/static/media/wood.bd1f3ced.png"
+  }
+]);
